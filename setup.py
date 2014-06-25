32e34e35e16ace9942243ca9c1bfd012546cf864
Alex Frolkin <a.frolkin@ocado.com>
2014-06-25 12:34:30 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
 	version = get_git_version(__file__),
 	description = 'JunOS-like shell',
 	author = 'InfDev',
-	author_email = 'infdev@ocado.com'
+	author_email = 'infdev@ocado.com',
+	py_modules = ['jsh']
 )
 

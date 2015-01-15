9b4080d45f7a6ac1993e5b02dfab959b977d5941
Alex Frolkin <a.frolkin@ocado.com>
2015-01-15 11:20:47 +0000

    Use new gitversion semantics

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 """Setup for JSH"""
 
 from distutils.core import setup
-from gitversion import get_git_version
+from jsh.version import __VERSION__
 
 setup(
 	name = 'jsh',
-	version = get_git_version(__file__),
+	version = __VERSION__,
 	description = 'JunOS-like shell',
 	author = 'InfDev',
 	author_email = 'infdev@ocado.com',
-	py_modules = ['jsh']
+	packages = ['jsh']
 )
 

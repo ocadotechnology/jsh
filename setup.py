36bc911ae6cf86f396c89277185970df82de6299
Alex Frolkin <a.frolkin@ocado.com>
2015-02-11 14:53:12 +0000

    Add readline dependency

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from distutils.core import setup
 from jsh.version import __VERSION__
 
 setup(
-	name = 'jsh',
-	version = __VERSION__,
-	description = 'JunOS-like shell',
-	author = 'InfDev',
-	author_email = 'infdev@ocado.com',
-	packages = ['jsh']
+	name			= 'jsh',
+	version			= __VERSION__,
+	description		= 'JunOS-like shell',
+	author			= 'InfDev',
+	author_email		= 'infdev@ocado.com',
+	packages		= [ 'jsh' ],
+	install_requires	= [ 'readline' ]
 )
 

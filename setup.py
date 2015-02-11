66c77f5feb3aac881ca635f2c7764ac58f9e03a3
Alex Frolkin <a.frolkin@ocado.com>
2015-02-11 16:03:33 +0000

    Fix dependencies

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from jsh.version import __VERSION__
 setup(
 	name			= 'jsh',
 	version			= __VERSION__,
-	description		= 'JunOS-like shell',
+	description		= 'JunOS-like shell library for Python',
 	author			= 'InfDev',
 	author_email		= 'infdev@ocado.com',
 	packages		= [ 'jsh' ],
-	install_requires	= [ 'readline' ]
+	install_requires	= [ 'readline' ] if platform.system() == 'Darwin' else []
 )
 

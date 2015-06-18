59424bcc31c33d4a5f3969cb638cca174a2192e2
Ben Cardy <ben.cardy@ocado.com>
2015-06-18 09:01:48 +0100

    Linting

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ from platform import system
 from jsh.version import __VERSION__
 
 setup(
-	name			= 'jsh',
-	version			= __VERSION__,
-	description		= 'JunOS-like shell library for Python',
-	author			= 'InfDev',
-	author_email		= 'infdev@ocado.com',
-	packages		= [ 'jsh' ],
-	install_requires	= [ 'readline' ] if system() == 'Darwin' else []
+    name='jsh',
+    version=__VERSION__,
+    description='Junos-like shell library for Python',
+    author='InfDev',
+    author_email='infdev@ocado.com',
+    packages=['jsh'],
+    install_requires=['readline'] if system() == 'Darwin' else [],
 )
-

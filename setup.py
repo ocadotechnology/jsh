893b3c9609dfbdfdb385021264d7fd69ebbbb635
Ben Cardy <ben.cardy@ocado.com>
2015-06-26 15:31:09 +0100

    Added basic classifiers

--- setup.py
+++ setup.py
@@ -12,4 +12,13 @@ setup(
     author_email='infdev@ocado.com',
     packages=['jsh'],
     install_requires=['readline'] if system() == 'Darwin' else [],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
